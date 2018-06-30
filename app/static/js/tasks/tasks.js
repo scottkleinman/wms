d689df62fa77aa09f9e1773b95779465b6e93beb
scottkleinman <scott.kleinman@csun.edu>
2018-06-29 21:44:42 -0700

    Complete tasks front end and test functions

--- app/static/js/tasks/tasks.js
+++ app/static/js/tasks/tasks.js
@@ -1,90 +1,138 @@
-/* This code has not yet been converted from Angular */
+// Configure the PySiQ server or use Flask for testing
+var queueServer = 'http://localhost:8081'
+queueServer = 'http://127.0.0.1:5000/tasks'
 
-var queue_server = "http://localhost:8081";
-  var app = angular.module("myTasksList", []);
-  app.controller("myCtrl", function($scope) {
-    var tasks = JSON.parse(sessionStorage.tasks || "[]");
-    $scope.tasks = [];
-    for (var i in tasks) {
-      $scope.tasks.push(tasks[i]);
-    }
-    $scope.checkStatus = function(task) {
-      // Send the GET request to server
-      if (task.status !== "FINISHED" && task.status !== "FAILED") {
-        $.ajax({
-          type: "GET",
-          url: queue_server + "/api/status/" + task.task_id,
-          success: function(response) {
-            if (response.success) {
-              task.status = response.status;
-              if (task.status === "FINISHED" || task.status === "FAILED") {
-                $scope.getResult(task);
-              }
-            } else {
-              task.status = "UNKNOWN";
-            }
-            $scope.$apply();
-          },
-          contentType: 'application/json'
-        });
-      }
-    };
-    $scope.getResult = function(task) {
-      // Send the GET request to server
-      $.ajax({
-        type: "GET",
-        url: queue_server + "/api/result/" + task.task_id,
-        success: function(response) {
-          if (response.success) {
-            task.result = response.result;
-          } else {
-            task.status = "UNABLE TO GET THE RESULT.";
+//
+// General Helper Functions
+//
+
+function jsonifyForm () {
+  // Handles JSON form serialisation
+  var form = {}
+  $.each($('form').serializeArray(), function (i, field) {
+    form[field.name] = field.value || ''
+  })
+  return form
+}
+
+// Check Status
+function checkStatus (task) {
+  // Send the GET request to server
+  if (task.status !== 'FINISHED' && task.status !== 'FAILED') {
+    $.ajax({
+      type: 'GET',
+      url: queueServer + '/api/status/' + task.task_id,
+      success: function (response) {
+        console.log(response)
+        response = JSON.parse(response)
+        if (response.success) {
+          var status = response.status
+          if (status === 'FINISHED' || status === 'FAILED') {
+            getResult(task)
           }
-          sessionStorage.tasks = JSON.stringify($scope.tasks);
-          $scope.$apply();
-        },
-        contentType: 'application/json'
-      });
-    };
-    $scope.addNewTask = function(task_name, n_seconds, task_result, depend, incompatible) {
-      // Send the POST request to server
-      $.ajax({
-        type: "POST",
-        url: queue_server + "/api/enqueue",
-        data: JSON.stringify({
-          fn: 'foo',
-          args: [n_seconds, task_result],
+          $('#task-' + task.task_id).html(response.status)
+          $('#taskResult-' + task.task_id).html(response.result)
+          $('#task-' + task.task_id).removeClass().addClass('label label-info')
+        } else {
+          $('#task-' + task.task_id).html(response.status)
+          $('#taskResult-' + task.task_id).html('UNKNOWN')
+          $('#task-' + task.task_id).removeClass().addClass('label label-warning')
+        }
+      },
+      contentType: 'application/json'
+    })
+  }
+}
+
+// Get Result
+function getResult (task) {
+  // Send the GET request to server
+  $.ajax({
+    type: 'GET',
+    url: queueServer + '/api/result/' + task.task_id,
+    success: function (response) {
+      response = JSON.parse(response)
+      if (response.success) {
+        task.result = response.result
+      } else {
+        task.status = 'UNABLE TO GET RESULT'
+      }
+      sessionStorage.tasks = JSON.stringify(tasks)
+    },
+    contentType: 'application/json'
+  })
+}
+
+// Add New Task
+function addNewTask () {
+  // Get the form vals
+  var formVals = jsonifyForm()
+  // Send the POST request to server
+  $.ajax({
+    type: 'POST',
+    url: queueServer + '/api/enqueue',
+    data: JSON.stringify(formVals),
+    success: function (response) {
+      response = JSON.parse(response)
+      if (response.success) {
+        tasks.push({
+          task_id: response.id,
+          task_name: formVals['task_name'],
           depend: [],
-          incompatible: []
-        }),
-        success: function(response) {
-          if (response.success) {
-            $scope.tasks.push({
-              task_id: response.task_id,
-              task_name: task_name,
-              depend: depend,
-              incompatible: incompatible,
-              status: "UNKNOWN",
-              result: "-"
-            })
-            $scope.$apply();
-            sessionStorage.tasks = JSON.stringify($scope.tasks);
-            alert("New task was succesfully enqueued!");
-          } else {
-            alert("Oops! Something went wrong when trying to enqueue your new task!");
-          }
-        },
-        contentType: 'application/json'
-      });
-    }
-    $scope.clearTasks = function() {
-      $scope.tasks = [];
-      sessionStorage.tasks = JSON.stringify($scope.tasks);
-    }
-    //Launch the intervar
-    setInterval(function() {
-      for (var i in $scope.tasks) {
-        $scope.checkStatus($scope.tasks[i])
+          incompatible: [],
+          status: response.status,
+          result: response.result
+        })
+        sessionStorage.tasks = JSON.stringify(tasks)
+        var row = '<tr id="' + response.id + '" class="taskrow">'
+        row += '<td>' + formVals['task_name'] + '</td>'
+        row += '<td id="task-' + response.id + '">' + response.id + '</td>'
+        row += '<td><span id="task-task.task_id" class="label label-primary">' + response.status + '</span></td>'
+        row += '<td><span id="taskResult-task.task_id" class="label label-primary">' + response.result + '</span></td>'
+        row += '<td><button type="button" data-delete="' + response.id + '" class="btn btn-sm btn-outline-editorial pull-right remove" title="Remove Task"><i class="fa fa-trash"></i></button></td>'
+        row += '</tr>'
+        $('#queueTable > tbody').append(row)
+        bootbox.alert('New task was succesfully enqueued!')
+      } else {
+        bootbox.alert('Oops! Something went wrong when trying to enqueue your new task!')
       }
-    }, 2000);
-  });
+    },
+    contentType: 'application/json'
+  })
+}
+
+// Clear Tasks
+function clearTasks () {
+  tasks = []
+  sessionStorage.tasks = JSON.stringify(tasks)
+}
+
+// Launch the interval
+setInterval(function () {
+  for (var i in tasks) {
+    checkStatus(tasks[i])
+  }
+}, 2000)
+
+$(document).ready(function () {
+  // Add New Task
+  $('#queuetask').click(function (e) {
+    e.preventDefault()
+    addNewTask()
+  })
+
+  // Clear Tasks
+  $('#clear').click(function (e) {
+    e.preventDefault()
+    clearTasks()
+    $('.taskrow').remove()
+  })
+})
+
+// Remove a Task
+$(document).on('click', '.remove', function () {
+  const removeId = $(this).attr('data-delete')
+  tasks.splice($.inArray(removeId, tasks), 1)
+  sessionStorage.tasks = JSON.stringify(tasks)
+  $('#' + removeId).remove()
+})

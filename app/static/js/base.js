03d0594409e5ab2b803293399b2e7c2ec9b9b275
scottkleinman <scott.kleinman@csun.edu>
2018-07-14 21:45:05 -0700

    Add processing icon

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,20 +1,28 @@
+/* Helper functions for the processing icon */
+function showProcessing () {
+  $('#processingIcon').show()
+}
+function hideProcessing () {
+  $('#processingIcon').delay(500).hide(50)
+}
+
 /* Scripts loaded by base.html */
-$(document).ready(function() {
-	// Disable search field
-	$('#search').submit(function(e) {
-		e.preventDefault();
-		alert('Search functionality has not yet been enabled.');
-	});
-	// Handle scroll to top
-	$(window).scroll(function(){ 
-		if ($(this).scrollTop() > 100) { 
-			$('#scroll').fadeIn(); 
-		} else { 
-			$('#scroll').fadeOut(); 
-		} 
-	}); 
-	$('#scroll').click(function(){ 
-		$("html, body").animate({ scrollTop: 0 }, 600); 
-		return false; 
-	});
-});
+$(document).ready(function () {
+  // Disable search field
+  $('#search').submit(function (e) {
+    e.preventDefault()
+    alert('Search functionality has not yet been enabled.')
+  })
+  // Handle scroll to top
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#scroll').fadeIn()
+    } else {
+      $('#scroll').fadeOut()
+    }
+  })
+  $('#scroll').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 600)
+    return false
+  })
+})
